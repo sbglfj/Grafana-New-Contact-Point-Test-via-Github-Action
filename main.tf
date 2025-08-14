f82cc0a2a227d7e56ef420f9e90928f31c04b97c
sbglfj <jpx2b1@gmail.com>
2025-08-14 14:57:46 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+   required_providers {
+      grafana = {
+         source  = "grafana/grafana"
+         version = ">= 2.9.0"
+      }
+   }
+}
+
+provider "grafana" {
+   
+   url   = "https://sbglfj.grafana.net"
+   auth  = "glsa_mNP6lEHszsvtIVFJzSAqlxXhQWzFh3ig_715c2eaa"
+}
